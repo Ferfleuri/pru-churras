44c330263d2f8ce67ad73c48a4061779fc76e1a0
Fernando Fleuri <fernandofleuri1@gmail.com>
2023-03-16 12:27:32 -0300

    terminando o projeto

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,34 +8,73 @@ import { Component } from '@angular/core';
 export class Tab2Page {
   valor = 1;
 
-  pont1 = 1;
-  pont3 = 3;
-  pont6 = 6;
-  pont9 = 9;
-  pont12 = 12;
 
-  prontotime1 = 0;
+  pontotime1 = 0;
   pontotime2 = 0;
+
+  vitoriatime1 = 0;
+  vitoriatime2 = 0;
 
   constructor() { }
 
-  ponto1() {
-    this.valor = this.pont1;
+  pontoTime1() {
+    this.valor = 1
   }
 
-  ponto3() {
-    this.valor = this.pont3;
+  pontoTime3() {
+    this.valor = 3
   }
 
-  ponto6() {
-    this.valor = this.pont6;
+  pontoTime6() {
+    this.valor = 6
   }
 
-  ponto9() {
-    this.valor = this.pont9;
+  pontoTime9() {
+    this.valor = 9
   }
 
-  ponto12() {
-    this.valor = this.pont12;
+  pontoTime12() {
+    this.valor = 12
   }
+
+  somarPontos() {
+
+    this.pontotime1 += this.valor
+    this.valor = 1
+    if (this.pontotime1 >= 13) {
+      this.vitoriatime1 + 1
+      this.pontotime1 = 0
+      this.pontotime2 = 0
+    }
+  }
+
+  subPontos() {
+
+    this.valor = 1
+    if (this.pontotime1 >= 1) {
+      this.pontotime1 -= this.valor
+    }
+  }
+
+
+  somarPontos2() {
+
+    this.pontotime2 += this.valor
+    this.valor = 1
+    if (this.pontotime2 >= 13) {
+      this.vitoriatime2 + 1
+      this.pontotime1 = 0
+      this.pontotime2 = 0
+    }
+  }
+
+  subPontos2() {
+
+    this.valor = 1
+    if (this.pontotime2 >= 1) {
+      this.pontotime2 -= this.valor
+    }
+  }
+
+
 }
