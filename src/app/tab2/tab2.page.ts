46e39e8534f7f7cd7c09ab3ec31e75f0c310af94
Fernando Fleuri <fernandofleuri1@gmail.com>
2023-03-16 08:56:28 -0300

    cintinuei o app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  valor = 1;
 
-  constructor() {}
+  pont1 = 1;
+  pont3 = 3;
+  pont6 = 6;
+  pont9 = 9;
+  pont12 = 12;
 
+  prontotime1 = 0;
+  pontotime2 = 0;
+
+  constructor() { }
+
+  ponto1() {
+    this.valor = this.pont1;
+  }
+
+  ponto3() {
+    this.valor = this.pont3;
+  }
+
+  ponto6() {
+    this.valor = this.pont6;
+  }
+
+  ponto9() {
+    this.valor = this.pont9;
+  }
+
+  ponto12() {
+    this.valor = this.pont12;
+  }
 }
